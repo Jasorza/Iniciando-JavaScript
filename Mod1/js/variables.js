c9baf0ba1e7185267ae2923e430138a59605573d
leydygamboa <dayiz9616@gmail.com>
2018-10-29 16:34:41 -0500

    Loool

---
+++ Mod1/js/variables.js
@@ -0,0 +1,54 @@
+//*Pedir Datos por teclado*//
+//*var nombre=prompt("Ingrese su Nombre");
+//*console.log("Este es mi Nombre",nombre);
+/** */alert(nombre);
+//** */var edad=prompt("Ingrese su edad");
+/*console.log("Esta es mi edad",edad);
+alert(edad);
+
+//*Variables Numericas*//
+var num=5;
+console.log("num", num);
+
+//**Variables de texto */
+var palabra="pablabra";
+console.log("palabra", palabra);
+
+//**Variables Booleanas */
+var activo=true;
+console.log("activo", activo);
+
+//**Variable tipo Arreglo */
+var colores=["red","cyan",activo, num];
+console.log("colores", colores);
+
+/**Variable tipo abjeto u objetos */
+var vehiculo={
+    tipo:"Automovil",
+    modelo:2018,
+    usado:true
+};
+console.log("vehiculo", vehiculo);
+console.log("tipo de vehiculo", vehiculo,tipo);
+
+
+const edad;
+edad=20;
+console.log("costantes edad", edad);
+/*edad=35;
+console.log("costantes edad", edad);
+
+/** */
+
+var anio=2018;
+var promedio=25;
+console.log("la concatenacion de "+anio+" y "+promedio+" es ", anio+Number(promedio));
+let num1=25;
+let num2=45;
+console.log("la concatenacion es", String(num1)+String(num2));
+//**Ventanas */
+var modelo=prompt("el modelo del carro es:", 2018); //**Valor por defecto */
+/**Alertas */
+/**Conf */
+var Conf=confirm("Desea salir de la App")
+console.log("Confirmacion", Conf);
